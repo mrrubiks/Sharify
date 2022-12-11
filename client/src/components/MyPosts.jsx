2a22f7fb76714500eb5f618da874cc6b95eedfbd
Nick Zhang <mrrubiks0418@gmail.com>
2022-12-10 21:45:06 -0500

    Modified website description

--- client/src/components/MyPosts.jsx
+++ client/src/components/MyPosts.jsx
@@ -1,30 +1,29 @@
-import MyNav from './subcomponents/Nav';
-import { useEffect, useState } from 'react';
-import PostContainer from './subcomponents/PostContainer';
+import MyNav from "./subcomponents/Nav";
+import { useEffect, useState } from "react";
+import PostContainer from "./subcomponents/PostContainer";
 import Axios from "axios";
 
 function Posts(props) {
-    
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    Axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/userposts`, {
+      withCredentials: true,
+    })
+      .then((res) => {
+        setPosts(res.data.posts);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    Axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/posts/user`,{ withCredentials: true })
-        .then((res) => {
-            setPosts(res.data.posts);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    }, []);
-    
-    return (
-            <main>
-                <MyNav />
-                <PostContainer posts={posts} socket={props.socket} />
-            </main>
-    );
-    
+  return (
+    <main>
+      <MyNav />
+      <PostContainer posts={posts} socket={props.socket} />
+    </main>
+  );
 }
 
 export default Posts;
